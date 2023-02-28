1e7b1b03cc8c8c3dfb9cf59859063949a2a96bee
Biruk Anley <birukanley2022@gmail.com>
2023-02-28 16:59:51 +0300

    update error hanlder

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,7 @@
 const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
+
+// function to handle errors
 const errorHandlerMiddleware = (err, req, res, next) => {
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message })
