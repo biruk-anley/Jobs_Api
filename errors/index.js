989de7a0e07f08dfa5ed65b89c1df58ab7508563
Biruk Anley <birukanley2022@gmail.com>
2023-02-28 16:59:37 +0300

    update index for error

--- errors/index.js
+++ errors/index.js
@@ -3,6 +3,8 @@ const UnauthenticatedError = require('./unauthenticated')
 const NotFoundError = require('./not-found')
 const BadRequestError = require('./bad-request')
 
+
+// export error handler
 module.exports = {
   CustomAPIError,
   UnauthenticatedError,
