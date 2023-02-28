b89014bab3da321b5569c28355b19fff2f0f168e
Biruk Anley <birukanley2022@gmail.com>
2023-02-28 16:59:44 +0300

    update not found

--- errors/not-found.js
+++ errors/not-found.js
@@ -1,6 +1,7 @@
 const { StatusCodes } = require('http-status-codes');
 const CustomAPIError = require('./custom-api');
 
+// handle not found error
 class NotFoundError extends CustomAPIError {
   constructor(message) {
     super(message);
